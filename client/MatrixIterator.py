6a00f888e0138d1c1fd2b63e309114738fbac63d
oliverjensen <oliver.damsgaard@gmail.com>
2017-05-11 11:11:42 -0700

    add improved iterator function

--- client/MatrixIterator.py
+++ client/MatrixIterator.py
@@ -4,7 +4,91 @@ class MatrixIterator:
 	def __init__(self):
 		pass
 
+	def clique_counter(self, g):
+	    count5 = 0
+	    count6 = 0
+	    count7 = 0
+	    count8 = 0
+	    count9 = 0
+	    count10 = 0
+	    sgsize = 10
+	    newsize = len(g)-sgsize
 
+	    ten_cliques = []
+	    for i in range(newsize + 1):
+	        for j in range(i + 1, newsize + 2):
+	            for k in range(j + 1, newsize + 3):
+	                i_to_j = g[i][j]
+	                i_to = g[i]
+	                j_to = g[j]
+	                if (i_to_j == i_to[k]) and (i_to_j == j_to[k]):
+	                    for l in range(k + 1, newsize + 4):
+	                        k_to = g[k]
+	                        if (i_to_j == i_to[l]) and (
+	                                    i_to_j == j_to[l]) and (
+	                                    i_to_j == k_to[l]):
+	                            l_to = g[l]
+	                            #print ([i, j, k, l])
+	                            for m in range(l + 1, newsize + 5):
+	                                if (i_to_j == i_to[m]) and (
+	                                            i_to_j == j_to[m]) and (
+	                                            i_to_j == k_to[m]) and (
+	                                            i_to_j == l_to[m]):
+	                                    m_to = g[m]
+	                                    count5 += 1
+	                                    #print ([i, j, k, l, m])
+	                                    for n in range(m + 1, newsize + 6):
+	                                        if (i_to_j == i_to[n]) and (
+	                                                    i_to_j == j_to[n]) and (
+	                                                    i_to_j == k_to[n]) and (
+	                                                    i_to_j == l_to[n]) and (
+	                                                    i_to_j == m_to[n]):
+	                                            n_to = g[n]
+	                                            count6 += 1
+	                                            for o in range(n + 1, newsize + 7):
+	                                                if (i_to_j == i_to[o]) and (
+	                                                            i_to_j == j_to[o]) and (
+	                                                            i_to_j == k_to[o]) and (
+	                                                            i_to_j == l_to[o]) and (
+	                                                            i_to_j == m_to[o]) and (
+	                                                            i_to_j == n_to[o]):
+	                                                    o_to = g[o]
+	                                                    count7 += 1
+	                                                    for p in range(o + 1, newsize + 8):
+	                                                        if (i_to_j == i_to[p]) and (
+	                                                                    i_to_j == j_to[p]) and (
+	                                                                    i_to_j == k_to[p]) and (
+	                                                                    i_to_j == l_to[p]) and (
+	                                                                    i_to_j == m_to[p]) and (
+	                                                                    i_to_j == n_to[p]) and (
+	                                                                    i_to_j == o_to[p]):
+	                                                            p_to = g[p]
+	                                                            count8 += 1
+	                                                            for q in range(p + 1, newsize + 9):
+	                                                                if (i_to_j == i_to[q]) and (
+	                                                                            i_to_j == j_to[q]) and (
+	                                                                            i_to_j == k_to[q]) and (
+	                                                                            i_to_j == l_to[q]) and (
+	                                                                            i_to_j == m_to[q]) and (
+	                                                                            i_to_j == n_to[q]) and (
+	                                                                            i_to_j == o_to[q]) and (
+	                                                                            i_to_j == p_to[q]):
+	                                                                    q_to = g[q]
+	                                                                    count9 += 1
+	                                                                    for r in range(q + 1, newsize + 10):
+	                                                                        if (i_to_j == i_to[r]) and (
+	                                                                                    i_to_j == j_to[r]) and (
+	                                                                                    i_to_j == k_to[r]) and (
+	                                                                                    i_to_j == l_to[r]) and (
+	                                                                                    i_to_j == m_to[r]) and (
+	                                                                                    i_to_j == n_to[r]) and (
+	                                                                                    i_to_j == o_to[r]) and (
+	                                                                                    i_to_j == p_to[r]) and (
+	                                                                                    i_to_j == q_to[r]):
+	                                                                            count10 += 1
+	                                                                            ten_cliques.append(
+	                                                                                [i_to_j, i, j, k, l, m, n, o, p, q, r])
+	    return count5, count6, count7, count8, count9, count10, ten_cliques
 
 	def is_counter_example(self, g, gsize):  # returns True if contains 10-Clique, along with the current number of 5 cliques                                                                                                                                                                                                                                                                                             
 		sgsize = 10                                                                                                                                                                                                                                                                                                                                                                                                 
